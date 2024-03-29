c2c0568e8d66193e96bc1c6b8a5fb4fbaf940640
FrogTheFrog <warliukz@gmail.com>
2024-03-29 16:17:07 +0200

    Issue

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <regex>
+#include <iostream>
+#include <locale>
+#include <chrono>
+
+int main() 
+{
+   std::setlocale(LC_ALL, ".UTF-8");
+
+   std::cout << "Before regex" << std::endl;
+
+   const auto now{std::chrono::steady_clock::now()};
+   std::regex rgx{"[a-z][a-z][a-z]"};
+
+   std::cout << "After regex. Took " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - now).count() << "ms" << std::endl;
+   
+   return 0;
+}
